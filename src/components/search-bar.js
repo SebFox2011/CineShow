41980dd34a1fd31014b23f0b060bf5840a250c76
Seb <sebastien.philippe5@gmail.com>
2019-08-07 09:48:38 +0200

    SearchBar: ajout de l'enregistrement du texte dans le input

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,10 +1,51 @@
 import React, {Component} from 'react'
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {searchText: "", placeholder: "Tapez votre film ..."};
+    }
+
+    handleChange(event) {
+        console.log(event.target.value);
+        this.setState({searchText: event.target.value});
+    }
+
     render() {
 
-        return <input/>
+        return (
+            <div>
+                <input onChange={this.handleChange.bind(this)} placeholder={this.state.placeholder}/>
+                <p>{this.state.searchText}</p>
+            </div>
+        )
     }
 }
 
 export default SearchBar;
+
+/*
+* import React,{Component}  from 'react'
+
+class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {searchText: "", placeholder: "Tapez votre film ..."};
+    }
+
+    handleChange(event) {
+        console.log(event.target.value);
+        this.setState({searchText: event.target.value});
+    }
+
+    render() {
+
+        return {
+            <input onChange={this.handleChange.bind(this)} placeholder={this.state.placeholder}/>
+        }
+    }
+
+}
+
+export default SearchBar;*/
