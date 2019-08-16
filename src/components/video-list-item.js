33fc75748a231f8fb873dbf897e73f40c0c76978
Seb <sebastien.philippe5@gmail.com>
2019-08-16 11:08:55 +0200

    Ajout du poster image pour chaque titre de film

--- src/components/video-list-item.js
+++ src/components/video-list-item.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
+const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
+
 const VideoListItem = ({movie}) => {
-    return <li>Un film recommandé : {movie.title}</li>
+    return <li>
+        <img height="100px" width="100px" src={`${IMAGE_BASE_URL}${movie.poster_path}`} alt="Une image du film"/>
+        <h3>{movie.title}</h3>
+    </li>
 }
 
 export default VideoListItem;
