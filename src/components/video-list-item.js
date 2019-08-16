dfa11b7c5323caf997bd156bc3c9301f4c59389f
Seb <sebastien.philippe5@gmail.com>
2019-08-16 12:08:29 +0200

    Ajout du composant video.js pour afficher la bande annonce Youtube du film mis en avant

--- src/components/video-list-item.js
+++ src/components/video-list-item.js
@@ -5,7 +5,9 @@ const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
 const VideoListItem = ({movie}) => {
     return <li>
         <img height="100px" width="100px" src={`${IMAGE_BASE_URL}${movie.poster_path}`} alt="Une image du film"/>
-        <h3>{movie.title}</h3>
+        <h3>{movie.title} id: {movie.id}</h3>
+        <p>Note moyenne: {movie.vote_average}</p>
+        <p>Synopsis: {movie.overview}</p>
     </li>
 }
 
--- src/containers/app.js
+++ src/containers/app.js
@@ -2,43 +2,62 @@ import React, {Component} from 'react';
 import SearchBar from '../components/search-bar'
 import VideoList from './video-list'
 import VideoDetail from '../components/video-detail'
+import Video from '../components/video'
+
 import axios from 'axios'
 
 const API_END_POINT = "https://api.themoviedb.org/3/";
 const POPULAR_MOVIE_URL = "discover/movie?language=fr&sort_by=popularity.desc";
 const API_KEY = "api_key=1a9538af85dc38a3b2ecc64c859fda68";
 
- class App extends Component {
-     constructor(props) {
-         super(props);
-         this.state={movieList:{},currentMovie:{}}
-     }
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {movieList: {}, currentMovie: {}}
+    }
 
-     componentWillMount () {
+    componentWillMount() {
         this.initMovies();
-     }
+    }
 
-     initMovies(){
-         axios.get(`${API_END_POINT}${POPULAR_MOVIE_URL}&${API_KEY}`).then(function(response){
-             this.setState({movieList:response.data.results.slice(1,6),currentMovie:response.data.results[0]});
-             console.log(this.state.currentMovie);
-         }.bind(this));
-     }
+    initMovies() {
+        axios.get(`${API_END_POINT}${POPULAR_MOVIE_URL}&${API_KEY}`).then(function (response) {
+            this.setState({movieList: response.data.results.slice(1, 6), currentMovie: response.data.results[0]},function (){
+                this.applyVideoToCurrentMovie();
+            });
 
-     render() {
+            //console.log(this.state.currentMovie);
+        }.bind(this));
+    }
 
-         const renderVideoList =() =>{
-             if (this.state.movieList.length >= 5){
-                 return <VideoList movieList={this.state.movieList}/>
-             }
-         };
+    applyVideoToCurrentMovie() {
+        axios.get(`${API_END_POINT}movie/${this.state.currentMovie.id}?${API_KEY}&append_to_response=videos&include_adult=false`).then(function (response) {
+            //console.log(response);
+            const youtubeKey = response.data.videos.results[0].key;
+            let newCurrentMovieState = this.state.currentMovie;
+            newCurrentMovieState.videoId = youtubeKey;
+            //console.log(youtubeKey);
+            this.setState({currentMovie:newCurrentMovieState});
+            console.log(this.state);
+        }.bind(this));
+
+    }
+
+    render() {
+
+        const renderVideoList = () => {
+            if (this.state.movieList.length >= 5) {
+                return <VideoList movieList={this.state.movieList}/>
+            }
+        };
         return (
 
-                <div>
-                    <SearchBar/>
-                    {renderVideoList()}
-                    <VideoDetail title={this.state.currentMovie.title} description={this.state.currentMovie.overview}/>
-                </div>
+            <div>
+                <SearchBar/>
+                {renderVideoList()}
+                <Video videoId={this.state.currentMovie.videoId}/>
+                <VideoDetail title={this.state.currentMovie.title} description={this.state.currentMovie.overview}/>
+            </div>
         );
     }
 }
